3725a4dd44c66f06e44d21ead0a2fa7607f6dbd2
HP <HP@DESKTOP-D4E4FAT>
2024-11-16 10:11:49 +0530

    2nd commit added

---
+++ src/main/java/gitPracticeSelenium/Demo.java
@@ -0,0 +1,13 @@
+package gitPracticeSelenium;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void sample()
+	{
+		System.out.println("This is the first commit");
+		System.out.println("Hellloo");
+	}
+
+}
